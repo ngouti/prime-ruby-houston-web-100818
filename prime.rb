2ca2a5ae079f2cc31b6834a059fbe7737dd842be
Emma Gouti <goutiemma@gmail.com>
2018-10-02 06:54:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+require 'benchmark'
+
+def prime?(x) 
+  if (x<=1)
+    return false;
+  elsif(x<=3)
+  return true;
+  elsif (x%2 == 0)
+    return false;
+  end
+
+i = 5;
+
+ while i*i <= x
+   if (x % i == 0 || x % (i+2) == 0)
+     return false;
+   end
+     i = i +6
+end
+  return true;
+end
+
